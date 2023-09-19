e83cef5dde346a0f0661920412f5f2935331172d
SWEET-creator <sosui0131@icloud.com>
2023-09-19 19:19:34 +0900

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "nextjstestacf92d20": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import { Amplify } from 'aws-amplify';
+import config from '../aws-exports';
+Amplify.configure(config);
 
 export default function Home() {
   return (
